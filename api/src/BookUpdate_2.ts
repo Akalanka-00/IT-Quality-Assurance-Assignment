f39ba68b2d187652634402fdfc7a71fa6d5ecae6
Monali Shiwanthika <105496631+Monali-MST@users.noreply.github.com>
2024-12-23 17:44:21 +0530

    add book update with user role check

--- api/src/BookUpdate_2.ts
+++ api/src/BookUpdate_2.ts
@@ -15,7 +15,17 @@ export class BookUpdate_2{
     }
 
     public async updateBookWithUser(){
-
+        const data = DataStore.getInstance().getData();
+        const book:Book = {
+            id: data.SharedData.randomInt,
+            title: `${data.SharedData.randomStr}_TiTle`,
+            author: `${data.SharedData.randomStr}_AUTHOR`
+        }
+        const response : ServerResponse = await this.requestHandler.putRequest(  UserRole.User,`/api/books/${book.id}`,book);
+        expect(response.status).toBe(403);
+        console.log(`Book : ${data.SharedData.randomStr}_TITLE Successfully Updated.`);
+        console.log(response);
+        expect(response.json).toBe("\"User is not permitted.\"");
     }
 
     public async updateBookWithNonIntegerID(){
