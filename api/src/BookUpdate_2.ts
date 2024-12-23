08c937af5d76fd8bd10d4302235d79f3b168cc3d
Monali Shiwanthika <105496631+Monali-MST@users.noreply.github.com>
2024-12-23 19:38:53 +0530

    add book update test cases - fix errors

--- api/src/BookUpdate_2.ts
+++ api/src/BookUpdate_2.ts
@@ -16,8 +16,25 @@ export class BookUpdate_2{
 
     public async updateBookWithUser(){
         const data = DataStore.getInstance().getData();
+        const bookCreate:Book = {
+            title: `${data.SharedData.randomStr}_Title_ToBeUPDATE`,
+            author: `${data.SharedData.randomStr}_Author_ToBeUPDATE`
+        }
+
+        const responseCreate:ServerResponse = await this.requestHandler.postRequest(
+            UserRole.Admin,
+            "/api/books",
+            bookCreate
+        );
+
+        expect(responseCreate.status).toBe(201);
+        expect(responseCreate.statusText).toBe('');
+        expect(responseCreate.json.title).toBe(`${data.SharedData.randomStr}_Title_ToBeUPDATE`);
+        expect(responseCreate.json.author).toBe(`${data.SharedData.randomStr}_Author_ToBeUPDATE`);
+        console.log(`Book Update: ${data.SharedData.randomStr}_TITLE book Successfully Saved to be updated.`);
+
         const book:Book = {
-            id: data.SharedData.randomInt,
+            id: responseCreate.json.id,
             title: `${data.SharedData.randomStr}_Title_UPDATED`,
             author: `${data.SharedData.randomStr}_Author_UPDATED`
         }
@@ -25,11 +42,12 @@ export class BookUpdate_2{
         const response : ServerResponse = await this.requestHandler.putRequest(
             UserRole.User,
             `/api/books/${book.id}`,
-            book);
+            book
+        );
 
         expect(response.status).toBe(403);
         expect(response.json).toContain("User is not permitted.");
-        console.log(`Book Update: ${book.title} book has Successfully Updated.`);
+        console.log(`Book Update: ${book.title} book could not be Updated with User Role.`);
     }
 
     public async updateBookWithNonIntegerID(){
@@ -43,17 +61,34 @@ export class BookUpdate_2{
         const response: ServerResponse = await this.requestHandler.putRequest(
             UserRole.Admin,
             `/api/books/${book.id}`,
-            book);
+            book
+        );
 
         expect(response.status).toBe(400);
-        //expect(response.json).toContain("Invalid ID format.");
         console.log(`Book Update: ${book.title} book could not be Updated with non-integer ID.`);
     }
 
     public async updateBookWithEmptyValues(){
         const data = DataStore.getInstance().getData();
-        const book: Book = {
-            id: data.SharedData.randomInt,
+        const bookCreate:Book = {
+            title: `${data.SharedData.randomStr}_Title_ToBeUPDATED`,
+            author: `${data.SharedData.randomStr}_Author_ToBeUPDATED`
+        }
+
+        const responseCreate:ServerResponse = await this.requestHandler.postRequest(
+            UserRole.Admin,
+            "/api/books",
+            bookCreate
+        );
+
+        expect(responseCreate.status).toBe(201);
+        expect(responseCreate.statusText).toBe('');
+        expect(responseCreate.json.title).toBe(`${data.SharedData.randomStr}_Title_ToBeUPDATED`);
+        expect(responseCreate.json.author).toBe(`${data.SharedData.randomStr}_Author_ToBeUPDATED`);
+        console.log(`Book Update: ${data.SharedData.randomStr}_TITLE book Successfully Saved to be updated.`);
+
+        const book:Book = {
+            id: responseCreate.json.id,
             title: "", // Empty title
             author: "" // Empty author
         };
