1ed65431632f71dad2cd76a012c832ff72307e7f
Shenal Akalanka <shenalakalanka513@gmail.com>
2024-12-28 19:59:10 +0530

    Revert "Hasintha dev"

--- api/utils/RequestHandler.ts
+++ api/utils/RequestHandler.ts
@@ -41,6 +41,7 @@ export class RequestHandler {
     private getHeader( userRole:UserRole ){
         const data = this.dataStore.getData();
         const headers = {};
+        let json;
         const username: string = userRole === UserRole.Admin ? data.Authentication.admin : data.Authentication.user;
         const password:string = data.Authentication.password;
 
@@ -48,13 +49,15 @@ export class RequestHandler {
         if(userRole !== UserRole.Unauthorized) {
             headers["Authorization"] = `Basic ${credentials}`
         }
+        json = JSON.stringify(headers);
+        console.log(`header: ${json}`)
         return headers;
 
     }
 
     private async getResponse(response:APIResponse){
         const contentType = response.headers()['content-type'] || '';
-        let json: string;
+        let json;
         if (contentType.includes('application/json')) {
             json = await response.json();
         } else {
