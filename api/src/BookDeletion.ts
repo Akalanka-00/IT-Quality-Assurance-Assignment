2baf2889d1dc63d3f2d3ce01c83c8d1856f36b28
Hasintha Methmal <rhmr1129@gmail.com>
2025-01-03 03:15:35 +0530

    BookDeletion

--- api/src/BookDeletion.ts
+++ api/src/BookDeletion.ts
@@ -2,47 +2,74 @@ import {APIRequestContext, expect} from "@playwright/test";
 import {RequestHandler} from "../utils/RequestHandler";
 import {ServerResponse} from "../model/ServerResponse";
 import {UserRole} from "../enum/UserRole";
+import {BookRetrieval} from "./BookRetrieval";
+import {DataStore} from "../utils/DataStore";
 
 export class BookDeletion{
 
     private request:APIRequestContext;
     private requestHandler:RequestHandler;
+    private bookRetrieval:BookRetrieval
 
     constructor(request:APIRequestContext){
         this.request = request;
         this.requestHandler = new RequestHandler(request);
+        this.bookRetrieval = new BookRetrieval(request);
     }
 
-    public async deleteBookWithInvalidParam(){
-        
-       
-        const param="INVALID";
-        const response:ServerResponse = await this.requestHandler.deleteRequest(UserRole.Admin, "/api/books",param);
-       
+    public async deleteBook(id:number, userRole:UserRole){
+        const response:ServerResponse = await this.requestHandler.deleteRequest(userRole, `/api/books/${id}`);
+        expect(response).toBeDefined();
+        return response;
+    }
+
+    public async checkBookDeletedByUser(response:ServerResponse, id:number){
+        expect(response.status).toBe(403);
+        expect(response.json).toBe("\"User is not permitted.\"");
+        console.log(`Book with ID ${id} could not be deleted by User.`);
+        const book = await this.bookRetrieval.retrieveBookById(UserRole.Admin, id);
+        expect(book).toBeDefined();
+        expect(book.id).toBe(id);
+    }
+
+    public async deleteBookWithNonExistingId(userRole:UserRole){
+        const data = DataStore.getInstance().getData();
+        const id = data.SharedData.randomInt;
+        const response:ServerResponse = await this.requestHandler.deleteRequest(userRole, `/api/books/${id}`);
+        expect(response).toBeDefined();
+        return response;
+    }
+
+    public async bookDeletionNotPermitted(response:ServerResponse){
+        expect(response.status).toBe(403);
+        expect(response.json).toBe("\"User is not permitted.\"");
+        console.log("Book could not be deleted by User.");
+    }
+
+    public async bookNotFound(response:ServerResponse){
+        expect(response.status).toBe(404);
+        expect(response.json).toBe("\"Book not found\"");
+    }
+
+    public async checkBookDeleteByAdmin(response:ServerResponse, id:number){
+        expect(response.status).toBe(200);
+        expect(response.json).toBe("\"\"");
+
+        console.log(`Book with ID ${id} deleted by Admin.`);
+        const book = await this.bookRetrieval.retrieveBookByNonExistID(UserRole.Admin, id);
+    }
+
+    public async deleteBookWithInvalidId(userRole:UserRole){
+        const id = "INVALID";
+        const response:ServerResponse = await this.requestHandler.deleteRequest(userRole, `/api/books/${id}`);
+        expect(response).toBeDefined();
+        return response;
+    }
+
+    public async checkBookDeletionWithInvalidId(response:ServerResponse){
         expect(response.status).toBe(400);
-        expect(response.json).toBe('""')
-        console.log("invalid param test passed");
+        expect(response.json).toBe('""');
     }
 
-    public async deleteBookWithNullParam(){
-        
-       
-        const param="";
-        const response:ServerResponse = await this.requestHandler.deleteRequest(UserRole.Admin, "/api/books",param);
-       
-        expect(response.status).toBe(405);
-        expect(response.json).toBe('""')
-        console.log("Null param test passed");
-    } 
-    
-    public async deleteBookWithValidParamAsAdmin(){
-
-        // const param="";
-        // const response:ServerResponse = await this.requestHandler.deleteRequest(UserRole.Admin, "/api/books",param);
-       
-        // expect(response.status).toBe(405);
-        // expect(response.json).toBe('""')
-        // console.log("Null param test passed");
-    }
 
 }
--- api/tests/features/bookDeletion.feature
+++ api/tests/features/bookDeletion.feature
@@ -4,26 +4,33 @@ Feature: Book Deletion
     Given When valid baseUrl is provided
 
 
-  Scenario:Delete a book with User Role
+  Scenario:Delete a book as User
     Given New book data is provided for delete book
     And Set User Role to User
     When I create a book
     Then the book should be created successfully
-    And I attempt to delete the same book
+    And I attempt to delete the book
     Then The book could not be deleted with User Role
 
-    Scenario: Update a book with valid param
-    Given Set User Role to Admin
-    When I attempt to delete the book with the valid param
+  Scenario: Delete a book with non existing id as User
+    Given Set User Role to User
+    When I attempt to delete the book with non existing id
     Then The delete should User is not permitted.
     
-    Scenario: Update a book with no existent param
+  Scenario: Delete a book with no existing param as Admin
     Given Set User Role to Admin
-    When I attempt to delete the book with no existent param
-    Then The delete should User is not permitted.
+    When I attempt to delete the book with non existing id
+    Then The delete should be book not found.
 
-    Scenario: Update a book with valid param
+  Scenario: Delete a book with valid param as Admin
+    Given New book data is provided for delete book
+    And Set User Role to Admin
+    When I create a book
+    Then the book should be created successfully
+    When I delete the book
+    Then The book should be deleted.
+
+  Scenario: Delete a book with invalid param as Admin
     Given Set User Role to Admin
-    When I attempt to delete the book with the valid param
-    Then The delete should User is not permitted.
-    
+    When I attempt to delete the book with invalid id
+    Then The delete should be invalid.
